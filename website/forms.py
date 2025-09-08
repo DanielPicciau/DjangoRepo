2ae555f23aa5a6ff83877e77f44e21cc380b1937
Daniel <151561451+DanielPicciau@users.noreply.github.com>
2025-09-08 10:16:59 +0100

    committed changes

--- website/forms.py
+++ website/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from django import forms 
-from dhango.forms.widgets import PasswordInput, TextInput
+from django.forms.widgets import PasswordInput, TextInput
 
 class CreateUserForm(UserCreationForm):
     class Meta:
--- website/views.py
+++ website/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render
-from forms import CreateUserForm, LoginForm
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/index.html')
 
 def login(request):
-    return render(request, 'pages/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'pages/login.html', {'form': form})
 
 def register(request):
-    form = CreateUserForm()
-    
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            # Redirect to the login page after successful registration
+            return redirect('login')
+    else:
+        form = CreateUserForm()
+
     context = {'form': form}
     return render(request, 'pages/register.html', context)
