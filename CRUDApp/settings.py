17515449d6bdeb16794c1cc748fc1936799f83c3
Daniel <151561451+DanielPicciau@users.noreply.github.com>
2025-09-15 15:09:53 +0100

    final push

--- CRUDApp/settings.py
+++ CRUDApp/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    'bootstrap5',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
